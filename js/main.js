0ab39760e105b9659fbf51a27ce810a7f66218b6
yujiseok <koesijuy@urbanisom.com>
2022-03-24 14:33:09 +0900

    done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger');
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItem = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if(!showMenu) {
-    hamburger.classList.add('open');
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItem.forEach((item) => item.classList.add("open"));
 
     showMenu = true;
   } else {
-    hamburger.classList.remove('open');
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItem.forEach((item) => item.classList.remove("open"));
 
     showMenu = false;
   }
 }
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
